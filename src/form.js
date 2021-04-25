c9f1ce17afca5b887788b4c787f479db3759cb55
Manny <55524897+tm-const@users.noreply.github.com>
2021-04-25 05:41:57 -0400

    Fixed IE11 button click issues

--- src/form.js
+++ src/form.js
@@ -1,6 +1,6 @@
 const steps = Array.from(document.querySelectorAll("form .step"));
-const nextBtn = document.querySelectorAll("form .next-btn");
-const prevBtn = document.querySelectorAll("form .previous-btn");
+const nextBtn = document.querySelectorAll(".dev-next-btn");
+const prevBtn = document.querySelectorAll(".dev-previous-btn");
 const form = document.querySelector("form");
 
 const results = [
@@ -13,26 +13,33 @@ const results = [
 
 nextBtn.forEach((button) => {
   button.addEventListener("click", () => {  
-    parent = button.parentNode;
-    div = parent.parentNode;
+    // Set parent node
+    divX = button.parentNode;
+    // Set child radio buttons with dynamic selector
+    elementDivX = '#'+divX.id+' input[name="'+divX.id+'"]'
 
-    const rbs = document.querySelectorAll('#'+div.id+' input[name="'+div.id+'"]');
+    // Select all radio elements with the id in current step ( IE11 : cleares Nodelist error )
+    var currentStepRadioButtons = Array.prototype.slice.call(document.querySelectorAll(elementDivX)); 
+    
     let selectedValue;
-    for (const rb of rbs) {
-        if (rb.checked) {
-            selectedValue = rb.value;
-            break;
-        }
+    for (const rbs of currentStepRadioButtons) {
+      // See if radio button is checked
+      if (rbs.checked) {
+          selectedValue = rbs.value;
+          break;
+      }
     }
 
     let errMsg = document.querySelector(".error-message");
+    // Toggle error message if selected values are all undefined : if is not undefined go to next step
     if (selectedValue == undefined) {
       errMsg.classList.remove("hide");
       errMsg.classList.add("show");
     } else {
       errMsg.classList.remove("show");
       errMsg.classList.add("hide");
-      changeStep("next");
+      
+      changeStep("next"); // Change next step
     }
   });
 });
@@ -45,24 +52,28 @@ prevBtn.forEach((button) => {
 
 // Submit pushes checked options into array and console log object
 form.addEventListener("submit", (e) => {
+
   e.preventDefault();
   const inputs = [];
+
   form.querySelectorAll("input[type='radio']:checked").forEach((input) => {
     const {
       name,
       value
     } = input;
+
     inputs.push({
       name,
       value
     });
   });
 
+  // loop through checked radio buttons and get values
   for (let i = 0; i < results.length; i++) {
     document.querySelector("."+results[i]).innerText = inputs[i].value;
   }
   console.log(inputs)
-  form.reset();
+  form.reset(); // reset form values
 });
 
 // Change to next or previous slide
@@ -71,6 +82,7 @@ function changeStep(btn) {
   const active = document.querySelector(".active");
   index = steps.indexOf(active);
 
+  // get URL param value
   let skipStep = getUrlParameter(window.location.href, "skip");
   skipStep--;
   
@@ -79,11 +91,18 @@ function changeStep(btn) {
   
   if (btn === "next") {
     index++;
+
+    // check if next step equals URL param value (++)
     if (index == skipStep) {
       index++;
     }
   } else if (btn === "prev") {
     index--;
+
+    // check if next step equals URL param value (--)
+    if (index == skipStep) {
+      index--;
+    }
   }
   
   steps[index].classList.add("fade-in");
